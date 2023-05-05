e04d88f124a61b1d2835e185b6d407af9737cc15
Nischal <kafle.nischal2021@gmail.com>
2023-05-05 19:21:19 +0545

    Commit latest on

--- src/Additem.js
+++ src/Additem.js
@@ -9,13 +9,11 @@ function Additem() {
  <img className="todoImg" src="./img/pic2.jpg"/>
  
     <label for="exampleFormControlInput1" class="form-label" style={{display: 'inline-block'}}>Add Your Items Here</label>
-   
-    <div className="inner">
-    <div className="input-container"></div>
-    <input type="Text" class="form-control" id="exampleFormControlInput1" placeholder="Add Item" />
-    <button class="btn " type="button">+</button>
+    
+<div className="relative"><input type="text"/>
+<button className="absolute">+</button>
+</div>
 
-    </div>
   </div>
  
   )
