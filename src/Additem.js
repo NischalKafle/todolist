57079bd7f85c99cf858dfc9d9d9c058ebf32a20e
Nischal <kafle.nischal2021@gmail.com>
2023-05-05 16:13:46 +0545

    New Commit

---
+++ src/Additem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+
+function Additem() {
+  return (
+ <div class="mb-3" id="main">
+ <img className="todoImg" src="./img/pic2.jpg"/>
+ 
+    <label for="exampleFormControlInput1" class="form-label" style={{display: 'inline-block'}}>Add Your Items Here</label>
+   
+    <div className="inner">
+    <div className="input-container"></div>
+    <input type="Text" class="form-control" id="exampleFormControlInput1" placeholder="Add Item" />
+    <i className="fa fa-plus add-btn"></i>
+    </div>
+  </div>
+ 
+  )
+}
+
+export default Additem
