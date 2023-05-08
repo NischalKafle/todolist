bf0cbe4c212fda4eb64c6cd0a9cd1b1521d28607
Nischal <kafle.nischal2021@gmail.com>
2023-05-08 13:21:47 +0545

    Redux Project-1

--- src/Additem.js
+++ src/Additem.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+import {incNumber} from "./actions";
+import {decNumber} from "./actions";
+
+import { useSelector, useDispatch } from "react-redux";
 
 
 function Additem() {
+  const changeTheNumber = useSelector(state => state.changeTheNumber);
+  
+  const dispatch = useDispatch();
+
   return (
  <div class="mb-3" id="main">
  <img className="todoImg" src="./img/pic2.jpg"/>
  
-    <label for="exampleFormControlInput1" class="form-label" style={{display: 'inline-block'}}>Add Your Items Here</label>
+    <label for="exampleFormControlInput1" className="form-label" style={{display: 'inline-block'}}>Add Your Items Here</label>
     
-<div className="relative"><input type="text"/>
-<button className="absolute">+</button>
+<div className="relative">
+<a className="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber())}><span>-</span></a>
+<input id="my-value" type="text" value={changeTheNumber}/>
+<a className="quantity__plus" title="Increment" onClick={() => dispatch(incNumber(5))}><span>+</span></a>
+<button id="absolute" type="button" className="btn">+</button>
 </div>
 
   </div>
