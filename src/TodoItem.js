1837183800faee0b5d177a44844abf7f742ca70c
Nischal <kafle.nischal2021@gmail.com>
2023-05-11 17:15:17 +0545

    Final commit done

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,8 +3,7 @@ const TodoItem=(props)=>{
 return(
     <div className="fly">
     <li>{props.listAdd}</li>
-    <i className="fas fa-2x fa-trash" onClick={
-        ()=>{
+    <i className="fas fa-2x fa-trash" onClick={()=>{
             props.deleteItems(props.id);
         }
     }></i>
